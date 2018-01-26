8352bfcf35b9eca4a669f88fdde6f0b451073c69
nviser <super_155305@ukr.net>
2018-01-26 19:20:58 +0200

    settings page made

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular'
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +8,44 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  melody: string = 'Выбрать мелодию';
+
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController) {
 
   }
 
+  chooseMelody() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Выбрать мелодию',
+      buttons: [
+        {
+          text: 'Melody1',
+          handler: () => {
+            this.melody = 'Melody1';
+          }
+        },
+        {
+          text: 'Melody2',
+          handler: () => {
+            this.melody = 'Melody2';
+          }
+        },
+        {
+          text: 'Melody3',
+          handler: () => {
+            this.melody = 'Melody3';
+          }
+        },
+        {
+          text: 'Отмена',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+ 
+    actionSheet.present();
+  }
 }
