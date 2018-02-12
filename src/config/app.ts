b825af4517679abede2f860906195e74a8f989f5
vitaliy <super_155305@ukr.net>
2018-02-12 17:58:12 +0200

    start make api get list

--- src/config/app.ts
+++ src/config/app.ts
@@ -6,3 +6,4 @@ export const API_LOGIN = "/index.php?route=client/auth/login";
 export const API_REG = "/index.php?route=client/auth/register";
 export const API_GET_CLIENT = "/index.php?route=client/auth/getClient";
 export const API_EDIT_CLIENT = "/index.php?route=client/auth/edit";
+export const API_CONTACTS = "/index.php?route=contacts/contacts/getContacts";
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ActionSheetController, App } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, ActionSheetController, App } from 'ionic-angular';
 import { PersonPage } from '../person/person';
 import { SettingsPage } from '../settings/settings';
 import { AboutPage } from '../about/about';
+
+import { ApiServiceProvider } from '../../providers/api-service/api-service';
 
 @IonicPage()
 @Component({
@@ -13,11 +15,36 @@ export class ListPage {
 
   kin: string = 'Степень родства';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private app: App, public actionSheetCtrl: ActionSheetController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    private app: App, public actionSheetCtrl: ActionSheetController,
+    public loadingCtrl: LoadingController, public apiServiceProvider: ApiServiceProvider
+  ) {
+    
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ListPage');
+  ionViewWillEnter() {
+    this.getUserData();
+  }
+
+  getUserData() {
+    let loading = this.loadingCtrl.create({
+        content: 'Загрузка данных пользователя...'
+    });
+    loading.present();
+
+    const client_id = localStorage.getItem('mb_client_id');
+
+    this.apiServiceProvider.get_user_data(client_id).then((data: any) => {
+    
+        /* this.userData.client_id = data.client_id;
+        this.userData.name = data.client_name;
+        this.userData.surname = data.client_surname;
+        this.userData.telephone = data.client_telephone;
+        this.userData.email = data.client_email;
+        this.userData.password = data.client_password; */
+
+        loading.dismiss();
+      });
   }
 
   goback() {
